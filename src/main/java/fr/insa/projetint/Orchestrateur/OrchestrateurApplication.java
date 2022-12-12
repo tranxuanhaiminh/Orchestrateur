da57697d416d39e7bf8f7354eea25cf4a0d3414a
xtran <xtran@etud.insa-toulouse.fr>
2022-12-12 11:33:25 +0100

    test orchestrateur

---
+++ src/main/java/fr/insa/projetint/Orchestrateur/OrchestrateurApplication.java
@@ -0,0 +1,25 @@
+package fr.insa.projetint.Orchestrateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class OrchestrateurApplication {
+	
+	RestTemplate restTemplate = new RestTemplate();
+
+	@RequestMapping("/")
+	public String home() {
+		String test1 = restTemplate.getForObject("http://localhost:8081", String.class);
+		return test1;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestrateurApplication.class, args);
+	}
+
+}
